53847b97bb50eaede767de6e405b0c5263a99a71
Rigamorphius <43024627+Rigamorphius@users.noreply.github.com>
2018-11-28 12:04:07 -0600

    Initial commit

---
+++ Helloworld/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld
+{
+    class Program
+    {
+      static void Main(string[] args)
+        {  
+
+            
+            
+            Console.WriteLine("Whats your Name?");
+String Name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + Name);
+            Console.ReadLine();
+        }
+
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("Whats your Name?");
+            String Name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + Name);
+            Console.ReadLine();
+        }
+    }
+}
